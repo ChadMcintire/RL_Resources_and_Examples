043fce0eb2a601891047c4940f865ab360d79687
ChadMcintire <chadmcintirecloud@gmail.com>
2022-08-26 11:11:04 -0600

    finished the training loop seems to be working as far as I can tell

--- soft_actor_critic_example/main.py
+++ soft_actor_critic_example/main.py
@@ -6,14 +6,18 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 import datetime
 from replay_memory import ReplayMemory
+import itertools
+
+
 
 def training_loop(args):
     #set up environment
     env = gym.make(args.env_name)
 
-    #set up seeding for env
+    #set up seeding
     env.seed(args.seed)
     env.action_space.seed(args.seed)
+
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
@@ -25,8 +29,84 @@ def training_loop(args):
     
     #Memory
     memory = ReplayMemory(args.replay_size, args.seed)
+
+    #Training Loop
+    total_numsteps = 0
+    updates = 0
+
+    for i_episodes in itertools.count(1):
+        episode_reward = 0
+        episode_steps = 0
+        done = False
+        state = env.reset()
+
+        while not done:
+            if args.start_steps > total_numsteps:
+                action = env.action_space.sample() #Sample random action
+            else:
+                action = agent.select_action(state) #Sample action from policy
+
+            if len(memory) > args.batch_size:
+                # Number of updates per step in environment
+                for i in range(args.updates_per_step):
+                    critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+
+                    writer.add_scalar("loss/critic_1", critic_1_loss, updates)
+                    writer.add_scalar("loss/critic_2", critic_2_loss, updates)
+                    writer.add_scalar("loss/policy", policy_loss, updates)
+                    writer.add_scalar("loss/entropy_loss", ent_loss, updates)
+                    writer.add_scalar("entropy_temperature/alpha", alpha, updates)
+                    updates += 1
+
+            next_state, reward, done, _ = env.step(action) # Step
+            episode_steps += 1
+            total_numsteps += 1
+            episode_reward += reward
+
+
+            # Ignore the "done" signal if it comes from hitting the time horizon.
+            # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+            mask = 1 if episode_steps == env._max_episode_steps else float(not done)
+
+            memory.push(state, action, reward, next_state, mask) # Append transition to memory
+
+            state = next_state
+
+
+        if total_numsteps > args.num_steps:
+            break
+
+        writer.add_scalar("reward/train", episode_reward, i_episodes)
+        #print every iteration
+        #print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episodes, total_numsteps, episode_steps, round(episode_reward, 2)))
+
+        if i_episodes % 100 == 0 and args.eval is True:
+            avg_reward = 0.
+            episodes = 10
+
+            for _ in range(episodes):
+                state = env.reset()
+                episode_reward = 0
+                done = False
+                while not done:
+                    action = agent.select_action(state, evaluation=True)
+
+                    next_state, reward, done, _ = env.step(action)
+                    episode_reward += reward
+
+                    state = next_state
+                avg_reward += episode_reward
+            avg_reward /= episodes
+
+            writer.add_scalar("avg_reward/test", avg_reward, i_episodes)
+
+            print("-------------------------------------------")
+            print("Test Episodes: {}, Avg. Reward: {} Test Episode Number for the next 10{}".format(episodes, round(avg_reward, 2), i_episodes))
+            print("----------------------------------------")
     
     
+    env.close()
+
     print("yes")
 
 
@@ -57,6 +137,12 @@ if __name__ == "__main__":
     parser.add_argument('--hidden_size', type=int, default=256, metavar='N', help='hidden size (default: 256)')
     parser.add_argument("--lr", type=float, default=0.0003, metavar="G", help="learning_rate (default: 0.0003)")
     parser.add_argument("--replay_size", type=int, default=1000000, metavar="N", help="size of replay buffer (default: 1000000)")
+    parser.add_argument("--start_steps", type=int, default=10000, metavar="N", help="Steps sampling random action (default: 10000)")
+    parser.add_argument('--batch_size', type=int, default=256, metavar='N', help='batch size (default: 256)')
+    parser.add_argument('--num_steps', type=int, default=1000001, metavar='N', help='maximum number of steps (default: 1000000)')
+    parser.add_argument('--eval', type=bool, default=True, help='Evaluates a policy a policy every 10 episode (default: True)')
+    parser.add_argument('--updates_per_step', type=int, default=1, metavar='N', help='model updates per simulator step (default: 1)')
+
     args = parser.parse_args()
 
     training_loop(args)
