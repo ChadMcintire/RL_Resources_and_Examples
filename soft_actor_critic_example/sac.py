fixed error, pathing was bad for saving and it was throwing a silent error

soft_actor_critic_example/sac.py
@@ -210,11 +210,12 @@ class SAC(object):
     """
     # Save model parameters
     def save_checkpoint(self, env_name, suffix="", ckpt_path="checkpoints", alpha=.2):
-        if not os.path.exists("check_points/" + ckpt_path + "/"):
-            os.makedirs("check_points/" + ckpt_path + "/")
+        ch_pt = "check_points/" 
+        if not os.path.exists(ch_pt + ckpt_path + "/"):
+            os.makedirs(ch_pt + ckpt_path + "/")
         #if ckpt_path is None:
-        ckpt_path = ckpt_path + "/sac_cp_{}_{}_{}_{}_{}".format("tuning=" + str(self.automatic_entropy_tuning), self.policy_type, env_name, alpha, suffix)
-        print("Save models to {}".format(ckpt_path))
+        ckpt_path = ch_pt + ckpt_path + "/sac_cp_{}_{}_{}_{}_{}".format("tuning=" + str(self.automatic_entropy_tuning), self.policy_type, env_name, alpha, suffix)
+        print("Save models to {}".format(ckpt_path + "/"))
         torch.save({"policy_state_dict": self.policy.state_dict(),
                     "critic_state_dict": self.critic.state_dict(),
                     "critic_target_state_dict": self.critic_target.state_dict(),